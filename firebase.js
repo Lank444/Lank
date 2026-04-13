37c1888054f2423b9569b1f3c71a7c163d6f9e20
Lank444 <saktigalang4@gmail.com>
2026-04-13 12:13:14 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbblC_BQSAbH5vI_Dqgr90KWnjQQjdmmY",
+  apiKey: "AIzaSyB57soTjoHei3M535jE_00F2NSzRXXIKdo",
   authDomain: "kopi-nusantara-61658.firebaseapp.com",
   databaseURL: "https://kopi-nusantara-61658-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "kopi-nusantara-61658",
   storageBucket: "kopi-nusantara-61658.firebasestorage.app",
   messagingSenderId: "20191939044",
-  appId: "1:20191939044:web:fe715323186062d96d9ff1",
-  measurementId: "G-8J574TN12B"
+  appId: "1:20191939044:web:aea22a05b5a556826d9ff1",
+  measurementId: "G-KPDMB2FV00"
 };
 
 const app = initializeApp(firebaseConfig);
